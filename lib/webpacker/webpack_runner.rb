9c06b76b8e8441556140fe7f91715c1eb8c9b206
Ersin Akinci <me@ersinakinci.com>
2018-08-04 18:51:08 -0700

    TEMP: --info-verbosity verbose

--- lib/webpacker/webpack_runner.rb
+++ lib/webpacker/webpack_runner.rb
@@ -5,7 +5,7 @@ module Webpacker
   class WebpackRunner < Webpacker::Runner
     def run
       env = { "NODE_PATH" => @node_modules_path.shellescape }
-      cmd = [ "#{@node_modules_path}/.bin/webpack", "--config", @webpack_config ] + @argv
+      cmd = [ "#{@node_modules_path}/.bin/webpack", "--config", @webpack_config, "--info-verbosity", "verbose", "--verbose" ] + @argv
 
       Dir.chdir(@app_path) do
         exec env, *cmd
